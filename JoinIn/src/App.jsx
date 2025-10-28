ee0ab347e83855b4a8e627b37b527b7ace2a3964
Kasra <golden.kasra@gmail.com>
2025-10-27 20:14:23 -0400

    Added Acount Creation Function

--- JoinIn/src/App.jsx
+++ JoinIn/src/App.jsx
@@ -6,7 +6,18 @@ import { use } from 'react'
 import axios from 'axios'
 function App() {
   const [data, setData] = useState()
-  useEffect(() => {
+  function createAccount() {
+    let accountObject={
+      name: "exampleName",
+      email: "exampleEmail",
+      password: "examplePassword",
+      dateCreated: new Date()
+    }
+    axios.post('http://localhost:3000/accounts/create', accountObject)
+  }
+
+
+  /*useEffect(() => {
     async function fetchData() {
       const response = await axios.get('http://localhost:3000/accounts');
       console.log(response.data);    
@@ -15,12 +26,13 @@ function App() {
       }
     }
     fetchData();
-  }, []);
+  }, []);*/
   return (
     <>
       <h1>JoinIn</h1>
       <h2>CURRENT DATABASE ENTRIES</h2>
       {JSON.stringify(data)} //Frontend backend connection verified
+      <button onClick={createAccount}>Login</button>
     </>
   )
 }
