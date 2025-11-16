eae3ebfa0a54e8245f7bcf444cfd6fbe715b23c5
vidovichandres <andres.vidovich@gmail.com>
2025-11-16 18:28:17 -0500

    Separated routes into controller + routes

---
+++ server/routes/postRoutes.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+const {
+    createPost,
+    getOnePost,
+    getAllPosts,
+    updatePost,
+    deletePost
+} = require('../controllers/postController.js')
+
+let postsRoutes = express.Router();
+
+
+
+// POST a new post
+postsRoutes.post('/posts/create', createPost)
+
+// GET a single post
+postsRoutes.get('/posts/:id', getOnePost)
+
+// GET all posts
+postsRoutes.get('/posts', getAllPosts)
+
+// PATCH a post
+postsRoutes.patch('/posts/update/:id', updatePost)
+
+// DELETE a post
+postsRoutes.delete('/posts/delete/:id', deletePost)
+
+
+module.exports = postsRoutes;
